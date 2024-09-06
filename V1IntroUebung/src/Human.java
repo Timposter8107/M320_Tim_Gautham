de373ea733f782146ba8cd3b98e8c6e9fb8073eb
Timposter8107 <tim.kunz@edu.tbz.ch>
2024-09-06 11:41:49 +0200

    V1IntroUebung hinzugefügt

---
+++ V1IntroUebung/src/Human.java
@@ -0,0 +1,15 @@
+public class Human {
+    private String name;
+
+    /**
+     * Constructor
+     */
+    public Human(String name) {
+        this.name = name;
+
+    }
+
+    public String getName() {
+        return this.name;
+    }
+}
